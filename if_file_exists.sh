196375b8da2a9d1d627f0d7af3f9196acc0e2dad
lopezlk <lopezlk@hotmail.com>
2016-05-11 20:27:39 -0400

    Tests and shell variable

---
+++ if_file_exists.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# 1 - This script shows the name of the script.
+# 2 - It needs a parameter .
+# 3 - It verifies if the parameter is an existing file.
+#
+echo "Script name $0 "  # Shell variable that contains the script name
+# Verifies if there's a parameter
+#
+if [ $# -ne 1 ]
+then
+        echo "Syntax : $0 filename"
+        exit 1 # No parameter given
+fi
+
+#Testing file existence 
+
+if [ -e "$1" ]
+then
+        file "$1"
+else
+        echo "File $1 doesn't exist"
+        exit 2 # File doesn't exist
+fi
